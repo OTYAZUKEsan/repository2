c1e6b5d5836f9428a09a3e61990cd79c4fa26f6e
OTYAZUKEsan <64253963+OTYAZUKEsan@users.noreply.github.com>
2021-03-03 13:19:19 +0900

    はじめてのこみっと

---
+++ ELReport/src/main/java/mrs/ElReportApplication.java
@@ -0,0 +1,13 @@
+package mrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElReportApplication.class, args);
+	}
+
+}
